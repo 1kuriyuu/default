380a95fd2d7bfcfdb6b833edac40f9d01d68e357
YUUMI4311 <s2522101@stu.musashino-u.ac.jp>
2026-01-09 14:24:35 +0900

    initial commit

---
+++ 天気予報app/main.py
@@ -0,0 +1,6 @@
+import flet as ft
+import requests
+from datetime import datetime
+
+AREA_URL = "https://www.jma.go.jp/bosai/common/const/area.json"
+FORECAST_URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/{}.json"
